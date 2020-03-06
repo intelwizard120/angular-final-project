610c0a750d31be926fc32ce73bedd5996cb48420
intelwizard120 <intelwizard120@gmail.com>
2020-03-05 21:39:06 -0500

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StartComponent } from './start/start.component';
+import { InterfaceComponent } from './interface/interface.component';
+import { RoadComponent } from './start/road/road.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StartComponent,
+    InterfaceComponent,
+    RoadComponent
   ],
   imports: [
     BrowserModule,
