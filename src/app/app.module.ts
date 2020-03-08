50377e13ef309b15c1493a365d2636898bdcd6cb
intelwizard120 <intelwizard120@gmail.com>
2020-03-08 17:52:02 -0400

    api stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { StartComponent } from './start/start.component';
 import { InterfaceComponent } from './interface/interface.component';
-import { RoadComponent } from './start/road/road.component';
+import { RoadComponent } from './interface/road/road.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import { RoadComponent } from './start/road/road.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
