6d7aac2c21a2e909f7c52a8b8bb5adf234b8339f
intelwizard120 <intelwizard120@gmail.com>
2020-03-09 18:05:29 -0400

    index.html: Added a google map to test to see how it looked and how to call it. Start componemnt: Added more changes to html and css to make it more unison. App component: Added a routes.

--- src/app/interactions.service.ts
+++ src/app/interactions.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Output } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
 @Injectable({
@@ -7,19 +7,18 @@ import { HttpClient } from "@angular/common/http";
 export class InteractionsService {
 
   private googleApiKey: string = "AIzaSyDZnC2DeWiqSRv6RDEQSl8VkjXr-HvKtGY";
-  private baseQueryUrl: string = "INSERTURL1";
-  private baseFilterUrl: string = "INSERTURL2";
+  private baseQueryUrl: string = "https://data.grandrapidsmi.gov/resource/ybyy-b267.json";
 
-
+  private baseFilterUrl: string = `${this.baseQueryUrl}//maps.googleapis.com/maps/api/js?key=${this.googleApiKey}`;
 
 
   constructor(private http: HttpClient) {
 
-    // Example of a method that gets a filtered api
-    // getFilteredSOMETHING() {
-    //   return this.http.get(
-    //     `${this.baseFilterUrl}&category=${category.category}&difficulty=${category.difficulty}&type=multiple`
-    //   );
-    //}
+
    }
+
+   getGoogleMap() {
+    let queryString = `${this.baseFilterUrl}`
+    return this.http.get(queryString);
+  }
 }
