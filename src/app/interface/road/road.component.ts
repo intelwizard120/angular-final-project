b90f9437ffd55b3b039bfc92caaccf69223b224c
intelwizard120 <intelwizard120@gmail.com>
2020-03-12 16:04:47 -0400

    Project before updating to Angular 9

--- src/app/interface/road/road.component.ts
+++ src/app/interface/road/road.component.ts
@@ -17,6 +17,15 @@ export class RoadComponent implements OnInit {
   zoom =14;
   reply: any
   mapType = 'roadmap';
+  markers = [
+    { lng: -85.6797945188957, lat: 42.95149365769928, alpha: 1 },
+    { lng: -85.67888678898416, lat: 42.95148546359977, alpha: 1 },
+    { lng: -85.67888678898416, lat: 42.95148546359977, alpha: 1 },
+    { lng: -85.67830904924729, lat: 42.95147479890077, alpha: 1 },
+    { lng: -85.67817861892253, lat: 42.951463101490894, alpha: 1 },
+    { lng:  -85.67794840391285, lat:  42.95140308170747, alpha: 1 },
+    { lng:  -85.67730775795953, lat:  42.951190967442535, alpha: 1 }
+  ];
  
 
   constructor(private Html : InteractionsService) { }
