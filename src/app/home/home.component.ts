4f4c748ddb6f297de97be472ef6c3fb2271edd39
intelwizard120 <intelwizard120@gmail.com>
2020-03-12 20:28:26 -0400

    refinding styling on roads page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,20 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  
   constructor() { }
-
+  
   ngOnInit() {
   }
-
+  
   moveBottom() {
     location.href = "#link311";
   }
-
+  
   moveTop() {
     location.href = "#home-content";
   }
-
-
-
+  
 }
