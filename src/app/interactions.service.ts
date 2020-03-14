3370c01ee88d4664402fb379cb1614728de5d509
intelwizard120 <intelwizard120@gmail.com>
2020-03-14 15:24:28 -0400

    working on adding search function

--- src/app/interactions.service.ts
+++ src/app/interactions.service.ts
@@ -6,25 +6,25 @@ import { HttpClient } from "@angular/common/http";
   providedIn: 'root'
 })
 export class InteractionsService {
-
-   cityUrl :string = 'https://data.grandrapidsmi.gov/resource/ce3k-dxxd.json/';
+  
+  cityUrl :string = 'https://data.grandrapidsmi.gov/resource/ce3k-dxxd.json/';
   private googleApiKey: string = "AIzaSyDZnC2DeWiqSRv6RDEQSl8VkjXr-HvKtGY";
   private baseQueryUrl: string = "https://data.grandrapidsmi.gov/resource/ybyy-b267.json";
-
-  private baseFilterUrl: string = `${this.baseQueryUrl}//maps.googleapis.com/maps/api/js?key=${this.googleApiKey}`;
-
-
-  constructor(private http: HttpClient) {
-
-    }
- getUrl(){
-      let response = this.cityUrl
-      return response
-   }
-
-   getCityData(){
-     return this.http.get(this.getUrl())
-     
-   }
   
+  private baseFilterUrl: string = `${this.baseQueryUrl}//maps.googleapis.com/maps/api/js?key=${this.googleApiKey}`;
+  
+  
+  constructor(private http: HttpClient) {
+    
   }
+  
+  getUrl(){
+    let response = this.cityUrl;
+    return response;
+  }
+  
+  getCityData(){
+    return this.http.get(this.getUrl())
+  }
+  
+}
--- src/app/interface/road/road.component.ts
+++ src/app/interface/road/road.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {InteractionsService} from '../../interactions.service'
 
+export interface ILocation {
+  newLatitude : number;
+}
 
 @Component({
   selector: 'app-road',
@@ -8,41 +11,39 @@ import {InteractionsService} from '../../interactions.service'
   templateUrl: './road.component.html'
 })
 
-
-
 export class RoadComponent implements OnInit {
-
+  
   latitude = 42.963795;
   longitude = -85.670006;
-  zoom =14;
-  reply: any
+  zoom = 14;
+  reply: any;
   mapType = 'roadmap';
+  
   markers = [
     { lng: -85.6797945188957, lat: 42.95149365769928, alpha: 1 },
     { lng: -85.67888678898416, lat: 42.95148546359977, alpha: 1 },
     { lng: -85.67888678898416, lat: 42.95148546359977, alpha: 1 },
     { lng: -85.67830904924729, lat: 42.95147479890077, alpha: 1 },
     { lng: -85.67817861892253, lat: 42.951463101490894, alpha: 1 },
-    { lng:  -85.67794840391285, lat:  42.95140308170747, alpha: 1 },
-    { lng:  -85.67730775795953, lat:  42.951190967442535, alpha: 1 }
+    { lng: -85.67794840391285, lat:  42.95140308170747, alpha: 1 },
+    { lng: -85.67730775795953, lat:  42.951190967442535, alpha: 1 }
   ];
- 
-
+  
+  
   constructor(private Html : InteractionsService) { }
-
-  ngOnInit() {
-
-} 
-
-// display(){
-//   let reply = this.Html.getCityData().subscribe((data) => this.reply = data)
-//   console.log(reply)
-// }
-   
   
-    
-   
+  ngOnInit() {  
+  } 
   
-   
+  // display(){
+  //   let reply = this.Html.getCityData().subscribe((data) => this.reply = data)
+  //   console.log(reply)
+  // }   
+
+  changeLocation (newLatitude) {
+    newLatitude = this.latitude; 
+    // newLongitude = this.longitude;
+    console.log(this.latitude);
+  }
   
 }
