e8f06d509af5b9841c476fa0041ffadd9f0f378b
intelwizard120 <intelwizard120@gmail.com>
2020-03-18 21:06:30 -0400

    worked on markers and polylines

--- src/app/interface/road/road.component.ts
+++ src/app/interface/road/road.component.ts
@@ -1,45 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import{Api311Service} from '../.././api-311.service'
+import{ApiPaserService} from '../.././api-paser.service'
+
 
 @Component({
-  selector: 'app-road',
-  styleUrls: ['./road.component.css'],
-  templateUrl: './road.component.html'
+  selector: "app-road",
+  styleUrls: ["./road.component.css"],
+  templateUrl: "./road.component.html"
 })
-
 export class RoadComponent implements OnInit {
-  
-  latitude : number = 42.963795;
-  longitude : number = -85.670006;
-  zoom : number = 14;
-  reply: any;
-  mapType : string = 'roadmap';
-  
-  markers = [
-    { lng: -85.6797945188957, lat: 42.95149365769928, alpha: 1 },
-    { lng: -85.67888678898416, lat: 42.95148546359977, alpha: 1 },
-    { lng: -85.67888678898416, lat: 42.95148546359977, alpha: 1 },
-    { lng: -85.67830904924729, lat: 42.95147479890077, alpha: 1 },
-    { lng: -85.67817861892253, lat: 42.951463101490894, alpha: 1 },
-    { lng: -85.67794840391285, lat:  42.95140308170747, alpha: 1 },
-    { lng: -85.67730775795953, lat:  42.951190967442535, alpha: 1 }
-  ];
-  
-  
-  constructor() { }
-  
-  ngOnInit() {  
-  } 
-  
-  // display(){
-  //   let reply = this.Html.getCityData().subscribe((data) => this.reply = data)
-  //   console.log(reply)
-  // }   
-  
-  changeLocation (latitude = 30, longitude = 25) {
-    this.latitude = Number(latitude); 
-    this.longitude = Number(longitude);
-    console.log(this.latitude);
-    console.log(this.longitude);
+  zoom = 11
+  // center: google.maps.LatLng
+  myLatLng= new google.maps.LatLng({lng:-85.6681, lat: 42.9634})
+  markers= []
+  polylines=[]
+  options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
   }
+
+  constructor(private api311: Api311Service, private apiPaser: ApiPaserService  ) {}
+
+  ngOnInit() {
+
+  }
+
+  addMarker(myLat:number, myLng:number) {
+    this.markers.push({
+      position: {
+        lat: myLat,
+        lng: myLng
+      },
+     
+      title: 'Marker title ' + (this.markers.length + 1),
+      
+    })
+  }
+  addPolyline(myLine: google.maps.Polyline) {
+    this.polylines.push(myLine)
+  }
+
   
+
+
+  click(event: google.maps.MouseEvent) {
+    
+    for(let place of this.api311.processCoordinates("January 1 2019")){
+      this.addMarker(place.lat, place.lng)
+    }
+    for(let place of this.apiPaser.processPolylines()){
+      this.addPolyline(place)
+  }}
+
+  // draw(event: google.maps.MouseEvent){
+  //   this.flightPath()
+  // }
+
+
 }
