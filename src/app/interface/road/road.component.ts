1c3645cc05c6f6ebb2ee065bbfdc703517b14154
intelwizard120 <intelwizard120@gmail.com>
2020-03-19 18:45:40 -0400

    polylines appear and have colors

--- src/app/interface/road/road.component.ts
+++ src/app/interface/road/road.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import{Api311Service} from '../.././api-311.service'
-import{ApiPaserService} from '../.././api-paser.service'
+import{Api311Service} from '../.././api-311.service';
+import{ApiPaserService} from '../.././api-paser.service';
 
 
 @Component({
@@ -8,12 +8,12 @@ import{ApiPaserService} from '../.././api-paser.service'
   styleUrls: ["./road.component.css"],
   templateUrl: "./road.component.html"
 })
+
 export class RoadComponent implements OnInit {
-  zoom = 11
-  // center: google.maps.LatLng
-  myLatLng= new google.maps.LatLng({lng:-85.6681, lat: 42.9634})
-  markers= []
-  polylines=[]
+  zoom = 11;
+  myLatLng = new google.maps.LatLng({lng:-85.6681, lat: 42.9634});
+  markers = [];
+  polylines = [];
   options: google.maps.MapOptions = {
     mapTypeId: 'hybrid',
     zoomControl: false,
@@ -21,9 +21,9 @@ export class RoadComponent implements OnInit {
     disableDoubleClickZoom: true,
     maxZoom: 15,
     minZoom: 8,
-  }
+  };
 
-  constructor(private api311: Api311Service, private apiPaser: ApiPaserService  ) {}
+  constructor(private api311 : Api311Service, private apiPaser : ApiPaserService) {}
 
   ngOnInit() {
 
@@ -35,30 +35,18 @@ export class RoadComponent implements OnInit {
         lat: myLat,
         lng: myLng
       },
-     
       title: 'Marker title ' + (this.markers.length + 1),
-      
-    })
+    });
   }
-  addPolyline(myLine: google.maps.Polyline) {
-    this.polylines.push(myLine)
-  }
-
-  
-
 
   click(event: google.maps.MouseEvent) {
-    
     for(let place of this.api311.processCoordinates("January 1 2019")){
-      this.addMarker(place.lat, place.lng)
+      //this.addMarker(place.lat, place.lng);
     }
-    for(let place of this.apiPaser.processPolylines()){
-      this.addPolyline(place)
-  }}
-
-  // draw(event: google.maps.MouseEvent){
-  //   this.flightPath()
-  // }
-
+    for(let lineyboi of this.apiPaser.processPolylines()){
+      this.polylines.push(lineyboi);
+      console.log(lineyboi.strokeColor);
+    }
+  }
 
 }
