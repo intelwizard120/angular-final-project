4528a169e6e038f179e210a79f547f76fbeeb257
intelwizard120 <intelwizard120@gmail.com>
2020-03-25 19:09:24 -0400

    fixed curly brace

--- src/app/interface/road/road.component.ts
+++ src/app/interface/road/road.component.ts
@@ -34,75 +34,70 @@ export class RoadComponent implements OnInit {
   filter: string = "road";
   visible;
   
-  constructor(
-    private api311: Api311Service,
-    private apiPaser: ApiPaserService,
-    private searchLocation: SearchLocationService
-    ) {}
-    
-    ngOnInit() {}
-    
-    addMarker(myLat: number, myLng: number) {
-      this.markers.push({
-        position: {
-          lat: myLat,
-          lng: myLng
-        },
-        title: "Marker title " + (this.markers.length + 1)
-      });
-    }
-    
-    click(event: google.maps.MouseEvent) {}
-    
-    // onClick(road, pothole) {
-    //   if (this.filter === "pothole") {
-    //     this.polylines = [];
-    //     for (let place of this.api311.processCoordinates("January 1 2019")) {
-    //       this.addMarker(place.lat, place.lng);
-    //     }
-    //   } else if (this.filter != "pothole") {
-    //     this.markers = [];
-    //     for (let lineyboi of this.apiPaser.processPolylines()) {
-    //       this.polylines.push(lineyboi);
-    //     }
-    //   }
-    // }
-    
-    toggleOnRoad:boolean = true;
-    toggleOnPothole:boolean = true;
-    radioRoad:boolean = true;
-    radioPothole:boolean = true;
-    
-    onClickRoad() {
-      if (this.toggleOnRoad) {
-        for (let lineyboi of this.apiPaser.processPolylines()) {
-          this.polylines.push(lineyboi);
-          this.radioRoad = false;
-        }
-        this.toggleOnRoad = false;
-      } else {
-        this.polylines = [];
-        this.toggleOnRoad = true;
-        this.radioRoad = true;
+  constructor( private api311: Api311Service, private apiPaser: ApiPaserService, private searchLocation: SearchLocationService ) {}
+  
+  ngOnInit() {}
+  
+  addMarker(myLat: number, myLng: number) {
+    this.markers.push({
+      position: {
+        lat: myLat,
+        lng: myLng
+      },
+      title: "Marker title " + (this.markers.length + 1)
+    });
+  }
+  
+  click(event: google.maps.MouseEvent) {}
+  
+  // onClick(road, pothole) {
+  //   if (this.filter === "pothole") {
+  //     this.polylines = [];
+  //     for (let place of this.api311.processCoordinates("January 1 2019")) {
+  //       this.addMarker(place.lat, place.lng);
+  //     }
+  //   } else if (this.filter != "pothole") {
+  //     this.markers = [];
+  //     for (let lineyboi of this.apiPaser.processPolylines()) {
+  //       this.polylines.push(lineyboi);
+  //     }
+  //   }
+  // }
+  
+  toggleOnRoad:boolean = true;
+  toggleOnPothole:boolean = true;
+  radioRoad:boolean = true;
+  radioPothole:boolean = true;
+  
+  onClickRoad() {
+    if (this.toggleOnRoad) {
+      for (let lineyboi of this.apiPaser.processPolylines()) {
+        this.polylines.push(lineyboi);
+        this.radioRoad = false;
       }
-    }
-    
-    onClickPothole() {
-      if (this.toggleOnPothole) {
-        for (let place of this.api311.processCoordinates("January 1 2019")) {
-          this.addMarker(place.lat, place.lng);
-        }
-        this.toggleOnPothole = false;
-        this.radioPothole = false;
-      } else {
-        this.markers = [];
-        this.toggleOnPothole = true;
-        this.radioPothole = true;
-      }
+      this.toggleOnRoad = false;
+    } else {
+      this.polylines = [];
+      this.toggleOnRoad = true;
+      this.radioRoad = true;
     }
   }
   
-  findAddress(){
+  onClickPothole() {
+    if (this.toggleOnPothole) {
+      for (let place of this.api311.processCoordinates("January 1 2019")) {
+        this.addMarker(place.lat, place.lng);
+      }
+      this.toggleOnPothole = false;
+      this.radioPothole = false;
+    } else {
+      this.markers = [];
+      this.toggleOnPothole = true;
+      this.radioPothole = true;
+    }
+  }
+  
+  findAddress() {
     this.searchLocation.getAddress(this.address).subscribe((data:any) => {
       this.center= data.results[0].geometry.location
       this.zoom = 16
